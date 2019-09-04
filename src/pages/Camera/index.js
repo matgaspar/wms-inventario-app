e143496be25f98ded0f32d1e454f365349366ac8
matgaspar <matheusgaspar13@hotmail.com>
2019-09-04 20:20:44 -0300

    added trying react-native-camera

--- src/pages/Camera/index.js
+++ src/pages/Camera/index.js
@@ -1,15 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { View, Alert, ActivityIndicator, TouchableOpacity } from "react-native";
+import { View, Alert, ActivityIndicator, TouchableOpacity, Text } from "react-native";
 import Icons from "react-native-vector-icons/Ionicons";
-import BarcodeScanner, {
-  Exception,
-  FocusMode,
-  CameraFillMode,
-  FlashMode,
-  pauseScanner,
-  resumeScanner
-} from "react-native-barcode-scanner-google";
 import api from '../../services/api';
+import { RNCamera } from 'react-native-camera';
 
 function Camera({ navigation }) {
   const [codigo, setCodigo] = useState(0);
@@ -44,46 +37,15 @@ function Camera({ navigation }) {
     }
   };
 
-  const handleBarcodeReader = ({ data, type }) => {
-    if (data) {
-      setCodigo(data);
-      pauseScanner()
-        .then(() => {
-          loadProduto(data);
-          // do something after the scanner (camera) stream was resumed.
-        })
-        .catch(e => {
-          resumeScanner();
-          // Print error if scanner stream could not be resumed.
-          console.log(e);
-        });
-    }
-    //console.log(`Barcode '${data}' of type '${type}' was scanned.`);
+  const handleBarcodeReader = async () => {
+    const options = { quality: 0.5, base64: true, skipProcessing: true, forceOrientation: true };
   };
 
   return (
     <View style={{ flex: 1 }}>
-      <BarcodeScanner
-        style={{ flex: 1 }}
-        onBarcodeRead={handleBarcodeReader}
-        onException={exceptionKey => {
-          // check instructions on Github for a more detailed overview of these exceptions.
-          switch (exceptionKey) {
-            case Exception.NO_PLAY_SERVICES:
-            // tell the user they need to update Google Play Services
-            case Exception.LOW_STORAGE:
-            // tell the user their device doesn't have enough storage to fit the barcode scanning magic
-            case Exception.NOT_OPERATIONAL:
-            // Google's barcode magic is being downloaded, but is not yet operational.
-            default:
-              break;
-          }
-        }}
-        focusMode={FocusMode.AUTO /* could also be TAP or FIXED */}
-        cameraFillMode={CameraFillMode.COVER /* could also be FIT */}
-        //barcodeType={BarcodeType.CODE_128 | BarcodeType.EAN_13 | BarcodeType.EAN_8 | BarcodeType.ALL /* replace with ALL for all alternatives */}
-        FlashMode={FlashMode.TORCH /* 0 is OFF or 1 is TORCH  */}
-      />
+      <TouchableOpacity onPress={handleBarcodeReader}>
+        <Text>Capturar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
